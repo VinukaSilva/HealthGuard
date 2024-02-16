676bf382986eb79302a7eb1d70ad3222de437ee0
Sandin Manmitha Maheeshakya <sadinkasm@gmail.com>
2024-02-16 20:31:32 +0530

    added resources and security measurements

--- login/js/main.js
+++ login/js/main.js
@@ -1,91 +1,94 @@
-
-
-function setCookie(uid,value,exp_days) {
+function setCookie(name, value, exp_days) {
     let d = new Date();
-    d.setTime(d.getTime() + (exp_days*24*60*60*1000));
+    d.setTime(d.getTime() + (exp_days * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toGMTString();
-    let user = $("#User").val();
-    document.cookies = user + "=" + value + ";" + expires + ";path=/";
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-(function ($) {
-    "use strict";
+function getCookie(name) {
+    let cookieName = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookies = decodedCookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
+}
 
+function generateCSRFToken() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+(function($) {
+    "use strict";
 
     /*==================================================================
     [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input100').each(function() {
+        $(this).on('blur', function() {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
-            }
-            else {
+            } else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
-  
-   
+
     /*=============== [ Validate ]  ===============*/
     let input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(event){
-        
+    $('.validate-form').on('submit', function(event) {
         $(".loader").css("visibility", "visible");
-        
         let check = true;
-
-        for(let i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (let i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
         let formData = {
             user: $("#User").val(),
             pass: $("#pass").val(),
-          };
+            _csrf: getCookie('_csrf')
+        };
 
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: "https://healthconnect-server.onrender.com/login",
             crossDomain: true,
             data: formData,
             dataType: "json",
             encode: true,
-          }).done(function (data) {
-            //console.log(data.patient[0]._id);
-            sessionStorage.setItem('user',$("#User").val());
-            sessionStorage.setItem('token',data.token);
-            //setCookie("uid", data.patient[0]._id, 1);
-            location.href = "../dashboard/patient/"
-          }).fail(function (data) {
-            for(let i=0; i<input.length; i++) {
+        }).done(function(data) {
+            sessionStorage.setItem('user', $("#User").val());
+            sessionStorage.setItem('token', data.token);
+            location.href = "../dashboard/patient/";
+        }).fail(function(data) {
+            for (let i = 0; i < input.length; i++) {
                 showValidate(input[i]);
-                check=false;
-
+                check = false;
             }
-          });
-      
-          event.preventDefault();
+        });
 
+        event.preventDefault();
         return check;
     });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function() {
+        $(this).focus(function() {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if((!$(input).attr('type') == 'text') || (!$(input).attr('name') == 'password')) {
+    function validate(input) {
+        if ((!$(input).attr('type') == 'text') || (!$(input).attr('name') == 'password')) {
             return true;
-        }
-        else {
-            if($(input).val().trim() == ''){
+        } else {
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -93,7 +96,6 @@ function setCookie(uid,value,exp_days) {
 
     function showValidate(input) {
         let thisAlert = $(input).parent();
-
         $(thisAlert).addClass('alert-validate');
         $(".alert-v").addClass('alert-validate-s');
         $(".loader").css("visibility", "hidden");
@@ -101,29 +103,29 @@ function setCookie(uid,value,exp_days) {
 
     function hideValidate(input) {
         let thisAlert = $(input).parent();
-
         $(thisAlert).removeClass('alert-validate');
         $(".alert-v").removeClass('alert-validate-s');
     }
-    
+
     /*==================================================================
     [ Show pass ]*/
     let showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
+    $('.btn-show-pass').on('click', function() {
+        if (showPass == 0) {
+            $(this).next('input').attr('type', 'text');
             $(this).find('i').removeClass('zmdi-eye');
             $(this).find('i').addClass('zmdi-eye-off');
             showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
+        } else {
+            $(this).next('input').attr('type', 'password');
             $(this).find('i').addClass('zmdi-eye');
             $(this).find('i').removeClass('zmdi-eye-off');
             showPass = 0;
         }
-        
     });
 
+    // Generate and set CSRF token
+    let csrfToken = generateCSRFToken();
+    setCookie('_csrf', csrfToken, 1);
 
 })(jQuery);
